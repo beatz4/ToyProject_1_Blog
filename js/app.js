16d9b68534100373063068f798ed65f4cd038a8a
jonghyeonNoh <beatz148@gmail.com>
2019-09-16 18:09:58 +0900

    stydtNodeJs

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + myDateTime());
+    fs.readFile('../index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 }).listen(8080);
+
+// http.createServer(function (req, res) {
+//   fs.readFile('../index.html', function(err, data) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(data);
+//     res.end();
+//   });
+// }).listen(8080);
+
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   // argument 1 : 200 means that all is OK
+//   // argument 2 : object containing the response headers
+//   var q = url.parse(req.url, true).query;
+//   var txt = q.year + " " + q.month;
+//   // res.write(req.url);
+//   res.end(txt);
+// }).listen(8080);
 
 myDateTime = function() {
     return Date();
