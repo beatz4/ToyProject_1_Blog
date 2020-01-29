5e0bc409b98e66a150c82fb37ed3383a8674111c
beatz4 <beatz4@naver.com>
2020-01-29 11:22:43 +0900

    modifyLoadHTML

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    document.getElementById("header").innerHTML = '<object type="text/html" data="header.html" width="100%"></object>';
-    // $("header").load("header.html");
+    document.getElementById("header").innerHTML = '<object type="text/html" data="header.html" width="100%" style="padding:0; margin:0"></object>';
+    // $("#header").load("header.html")
 });
 
