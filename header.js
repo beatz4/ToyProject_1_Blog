30234376eeac5e76b17d128cf2b41e7d6c81f320
beatz4 <beatz4@naver.com>
2020-01-29 11:24:17 +0900

    useJSFile

--- header.js
+++ header.js
@@ -1,5 +1,9 @@
-$(document).ready(function() {
-    document.getElementById("header").innerHTML = '<object type="text/html" data="header.html" width="100%" style="padding:0; margin:0"></object>';
-    // $("#header").load("header.html")
-});
+// $(document).ready(function() {
+//     document.getElementById("header").innerHTML = '<object type="text/html" data="header.html" width="100%" style="padding:0; margin:0"></object>';
+//     // $("#header").load("header.html")
+// });
 
+
+$(document).ready( function() {
+    $("#header").load("header.html");
+  });
